3d1cf62b426063152fad7afbfbb6a2a34a623f00
Dhiraj Dhiman <dhirajdhiman15@gmail.com>
2025-08-21 01:06:47 +0530

    refactor: remove unused Suspense component from Browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -2,7 +2,7 @@
 
 import FilterSidebar from "./components/FilterSidebar";
 import ExperiencesList from "./components/ExperiencesList";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 // export interface Experience {
 //     id?: string;
 //     title?: string;
@@ -33,10 +33,8 @@ export default function Browse() {
                 <FilterSidebar />
 
                 <section>
-                    <Suspense fallback={<p className="text-gray-400">Loading experiences...</p>}>
-                        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
-                        <ExperiencesList />
-                    </Suspense>
+                    {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+                    <ExperiencesList />
                 </section>
             </div>
         </main>
