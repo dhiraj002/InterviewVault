cc1db6cbb500d7a038c27785d02b706b8cbd8d29
Dhiraj Dhiman <dhirajdhiman15@gmail.com>
2025-08-21 01:12:53 +0530

    fix: enable Suspense component for loading state in Browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -2,7 +2,7 @@
 
 import FilterSidebar from "./components/FilterSidebar";
 import ExperiencesList from "./components/ExperiencesList";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 // export interface Experience {
 //     id?: string;
 //     title?: string;
@@ -28,15 +28,17 @@ export const metadata: Metadata = {
 
 export default function Browse() {
     return (
-        <main className="bg-[#0d1117] min-h-screen py-12 px-4 text-gray-200">
-            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[250px_1fr] gap-8">
-                <FilterSidebar />
+        <Suspense fallback={<p className="text-gray-400">Loading experiences...</p>}>
+            <main className="bg-[#0d1117] min-h-screen py-12 px-4 text-gray-200">
+                <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-[250px_1fr] gap-8">
+                    <FilterSidebar />
 
-                <section>
-                    {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
-                    <ExperiencesList />
-                </section>
-            </div>
-        </main>
+                    <section>
+                        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
+                        <ExperiencesList />
+                    </section>
+                </div>
+            </main>
+        </Suspense>
     );
 }
